6f049122efc120400ef3403099e5a8ccc8bee49a
Sandeep Suthar <ss.suthar.004@gmail.com>
2025-07-10 14:14:48 +0530

    commit

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "sandeep@gmail.com"
+var accountPassword = "12345"
+accountCily = "Vadodra"
+
+let accountState;
+//accountId = 2  // not allowed
+
+
+accountEmail = "ss@ss.com"
+accountPassword = "2121212121"
+accountCity = "Ahamdabad"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
