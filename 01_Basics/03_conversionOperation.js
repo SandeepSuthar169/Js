e8b5645c61d8a946e2cffdf17ab8d482d7f56f2e
Sandeep Suthar <ss.suthar.004@gmail.com>
2025-07-11 10:11:12 +0530

    commit

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score = "sandeep"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNuber = Number(score)
+// console.log(typeof valueInNuber);
+// console.log(valueInNuber);
+
+// "33" -> 33
+// "33abc" -> NaN
+// true -> 1, false -> 0
+
+let isLoggedIn = "sandeep"
+let boolenIsLoggenIn = Boolean(isLoggedIn)
+// console.log(boolenIsLoggenIn);
+
+// 1 -> true, 0 -> false
+// "" -> false
+// "sandeep" -> true
+
+let someNumber = 33
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
