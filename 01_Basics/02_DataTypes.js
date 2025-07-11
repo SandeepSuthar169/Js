a74e22183cebc27a0b0039bead131ed774119378
Sandeep Suthar <ss.suthar.004@gmail.com>
2025-07-11 08:41:39 +0530

    commit

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; //newer Js code
+
+
+// alert(3 + 3)   // we are using nodejs , not 
+
+
+
+console.log(3+ 3); 
+console.log("Sandeep");
+
+
+let name = "sandeep"
+let age = 21
+let isLoggeIN  = false
+let state;
+
+// number -> 2**53
+//bigint
+// string -> ""
+// boolean -> true/false
+//null -> standalone value
+//undefined -> 
+//symbol -> unique
+
+
+// object
+
+console.log(typeof null);        // object
+console.log(typeof undefined);   // underfiend
