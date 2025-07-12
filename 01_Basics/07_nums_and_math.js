a77a9ae00de51523c5747c1a72afb3fafbe034f5
Sandeep Suthar <ss.suthar.004@gmail.com>
2025-07-12 11:02:22 +0530

    commit

---
+++ 01_Basics/07_nums_and_math.js
@@ -0,0 +1,32 @@
+const score = 100
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 1123.856657 
+
+// console.log(otherNumber.toPrecision(3))    //* IMP 
+
+
+const oneTwo = 10000000000
+// console.log(oneTwo.toLocaleString('en-IN'));     
+
+// ==============  Maths =======================
+
+// console.log(Math);
+// console.log(Math.abs(-4));            // 4
+// console.log(Math.round(-4.788776));   // -> -5
+// console.log(Math.ceil(4.188776));     //-> 5
+// console.log(Math.floor(4.99999));    // -> 4 
+// console.log(Math.min(4, 2,46,6,4,3,6));    // ->  2
+// console.log(Math.max(4, 2,46,6,4,3,6));    // ->  46
+
+console.log(Math.random());  // 0 to  1
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
