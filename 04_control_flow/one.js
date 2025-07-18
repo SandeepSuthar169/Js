2322d1e12b44ca92e82f398ba2d3fbfeb7267244
Sandeep Suthar <ss.suthar.004@gmail.com>
2025-07-18 13:22:11 +0530

    Commit

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -1,7 +1,61 @@
 // if 
 
-const isUserLoggedIn = true
+// const isUserLoggedIn = true
+// const temperature = 99
 
-if (3 != 2) {
+// if (temperature < 50) {
+//     console.log("less than 50");
+// } else {
+//     console.log("temperature is greater than 50");
+// }
+// console.log("execute");
 
+
+// <, >, <=, >=, =,  ==, ===, !=, !== -------------- IMP
+
+
+// SCOPE --------------------------------------------------IPM
+
+// const score = 200
+
+// if (score > 100) {
+//     const power = "fly"
+//     console.log(`user power : ${power} `);
+// }
+
+// console.log(`user power : ${power}`);
+
+
+const balance = 1000
+
+// if (balance > 500) console.log("test");
+
+// ------------------------------------------------------------------------------
+// if (balance < 500){
+//     console.log("less than 500");
+
+// } else if (balance < 750) {
+//     console.log("less than 750");
+
+// } else if (balance < 900) {
+//     console.log("less than 900");    
+
+// } else {
+//     console.log("less than 1200");
+// }
+ 
+// -----------------------------------------------------------------------------
+const User_LoggedIN = true
+const debitCard = true
+const loggedIn_Form_Google = false
+const loggedIN_Form_Email = true
+
+if (User_LoggedIN && debitCard && 2==3) {
+    console.log("allow to shoping");
+}
+
+
+if (loggedIn_Form_Google || loggedIN_Form_Email) {
+    console.log("user logged in");
+    
 }
