94d66ae9b91f65de572ccfe749c82076ab09d7e2
Sandeep Suthar <ss.suthar.004@gmail.com>
2025-07-19 09:19:44 +0530

    commit

---
+++ 05_Iterations_Loop.js/02_while_do_while.js
@@ -0,0 +1,22 @@
+// 1 ------------------------
+let i = 0
+while (i <= 10) {
+    // console.log(`${i}`);
+    i = i + 2
+    
+}
+
+// 2 -----------------------------------------
+let myArray = ["a", "b", "c", "d", "e", "z"]
+let g = 0
+while (g < myArray.length) {
+    // console.log(` values = ${myArray[g]}`);
+    g = g + 1    
+}
+
+// 3 -----------------------------------------
+let m = 11
+do {
+    console.log(`${m}`);
+    m++
+} while (m <= 10);
