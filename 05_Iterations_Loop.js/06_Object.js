c603be1ea9e8ec75f68c711a2359638b7cdee73d
Sandeep Suthar <ss.suthar.004@gmail.com>
2025-07-19 11:46:09 +0530

    commit

--- 05_Iterations_Loop.js/06_Object.js
+++ 05_Iterations_Loop.js/06_Object.js
@@ -39,9 +39,62 @@ map.set('IN',  "india")
 
 
 for (const key in map) {
-    console.log(key);            // it is not a iterable 
+    // console.log(key);            // it is not a iterable 
     
 }
 
 
+// 4----------------------------------------------
+const coding = ['js', 'py', 'php', 'java', 'C++', 'C#']
+//4.1
+coding.forEach( function (i) {
+    // console.log(i);
+    
+})
+//4.2
+coding.forEach( (i) => {
+    // console.log(i);
+    
+})
 
+//4.3
+function printMe(i) {
+    console.log(i);
+    
+}
+// coding.forEach(printMe)
+
+
+
+//4.4
+coding.forEach( (i, index, arr ) => {
+    // console.log(i, index, arr);
+    
+} )
+
+
+
+//5 ------------------------------------------------
+// [{}, {}, {}]
+
+const my_Coding = [
+    {
+        language_name: "AS",
+        last_name: "BS"
+    },
+    {
+        language_name: "CW",
+        last_name: "BW"
+    },
+    {
+        language_name: "RT",
+        last_name: "YT"
+    },
+]
+
+my_Coding.forEach( (i) => {
+    
+    console.log(i.language_name);
+    console.log(i.last_name);
+    
+} )
