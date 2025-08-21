f148fc3d7829245ad675c1adf771b67258867ca1
Sandeep Suthar <ss.suthar.004@gmail.com>
2025-08-21 16:59:13 +0530

    keybord-project

---
+++ 08_Events/keybord-project/scripts.js
@@ -0,0 +1,18 @@
+const insert = document.getElementById('insert')
+
+window.addEventListener('keydown', function(e){
+    insert.innerHTML = `
+        <div class='color'>
+        <table>
+                <tr>
+                    <th>key</th>
+                    <th>Code</th>
+                </tr>
+                <tr>
+                    <td>${e.key === " " ? "Space": e.key }</td>
+                    <td>${e.code}</td>
+                </tr>
+                </table>
+        </div>
+    `
+})
