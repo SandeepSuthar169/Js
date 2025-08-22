59a211b92e9b1e7019ee8c87b3103752feb5be82
Sandeep Suthar <ss.suthar.004@gmail.com>
2025-08-22 13:01:10 +0530

    Promises: error and json file handle

--- 09_Promises/one.js
+++ 09_Promises/one.js
@@ -107,17 +107,17 @@ consumePromiseFive()
 
 
 //----------------------------------------------------------------    IMP
-// async function getAllUser(){
-//     try {
-//         const response = await fetch('https://jsonplaceholder.typicode.com/users')
-//         const data = await response.json();
-//         console.log(data);
-//     } catch (error) {
-//         console.log("Error", error);   
-//     }
-// }
+async function getAllUser(){
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.log("Error", error);   
+    }
+}
 
-// getAllUser()
+getAllUser()
 
 //  ++++++++++++++++++++ useing to .them(), .catch(), .finally()  ++++++++++++++    IMP
 
