fc26f90890619b8a78647afb72d8cb542936d53e
Sandeep Suthar <ss.suthar.004@gmail.com>
2025-08-24 13:01:37 +0530

    commit files

---
+++ OOps.js
@@ -0,0 +1,13 @@
+const user = {
+    userName: "sandeep",
+    loginCount: 7,
+    sigedIn: true, 
+
+    getUserDatails: function(){
+        console.log('jskdf ksjhfjks sdjhfkjs shdkfjs');
+        
+    }
+}
+
+console.log(user.userName);
+console.log(user.getUserDatails());
