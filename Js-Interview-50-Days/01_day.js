df4e404e6a649afc03a637a58741237d04967ba6
Sandeep Suthar <ss.suthar.004@gmail.com>
2025-09-09 18:49:32 +0530

    Interview question

--- Js-Interview-50-Days/01_day.js
+++ Js-Interview-50-Days/01_day.js
@@ -45,7 +45,43 @@ const countChar = function (name, namechar)  {
     
 }
 
-console.log(countChar("MiscInjhfitfjexcfjIwi", "I"));
+// console.log(countChar("MiscInjhfitfjexcfjIwi", "I"));
 
 
 //4 ------------------------------------------------------------------------------------
+
+const checkTraingleType = function(a, b, c){
+    if(a === b && b === c){
+        return "equilatral"
+
+    } else if (a === b || a === c || b === c) {
+        return "isoscels"
+
+    } else {
+        return "scalence"
+
+    }
+}
+
+// console.log(checkTraingleType(3, 3, 3));
+// console.log(checkTraingleType(3, 4, 3));
+// console.log(checkTraingleType(6, 1, 3));
+
+//5 -----------------------------------------------------------------------------------
+
+const sortAscendings = (arr) => {
+
+    for(let i = 0; i < arr.length; i++){
+        for(let j = 0; j < i; j++){
+            if(arr[i] < arr[j]){
+                let temp = arr[i]
+                arr[i] = arr[j]
+                arr[j] = temp
+            }
+        }
+    } 
+    return arr
+}
+    
+
+console.log(sortAscendings([4, 3, 7, 6, 10]));
