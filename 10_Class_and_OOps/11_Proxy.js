c1b91e93136d6a029a04b04adeb7d2aaa98b660f
Sandeep Suthar <ss.suthar.004@gmail.com>
2025-09-20 06:10:57 +0530

    proxy question

--- 10_Class_and_OOps/11_Proxy.js
+++ 10_Class_and_OOps/11_Proxy.js
@@ -1,33 +1,32 @@
-const user = {
-    name: "sandeep",
-    age: 21,
-    password: "8192324"
-}
+// const user = {
+//     name: "sandeep",
+//     age: 21,
+//     password: "8192324"
+// }
 
-const proxyUser = new Proxy(user, {
-    get(target, prop){
-        console.log(target);
-        console.log(prop);
+// const proxyUser = new Proxy(user, {
+//     get(target, prop){
+//         console.log(target);
+//         console.log(prop);
 
-        if(prop === "password"){
-            throw new Error("Access Denied")
-        }
-        return target[prop]
-    },
+//         if(prop === "password"){
+//             throw new Error("Access Denied")
+//         }
+//         return target[prop]
+//     },
 //---------------------------------------------
-    set(target, prop, user){
+//     set(target, prop, user){
 
-    }
+//     }
 
-})
+// })
 
 // console.log(proxyUser.name);
 // console.log(proxyUser.password);
 
 
-//================================================================
-//================================================================
 
+//----------------------------------------------------------------
 function negativeIndex(array){
     return new Proxy(array, {
 
@@ -54,6 +53,36 @@ function negativeIndex(array){
 let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 let newArray = negativeIndex(array)
 
-console.log(array[-1]);
-console.log(newArray[-4]);
+// console.log(array[-1]);
+// console.log(newArray[-4]);
+
+
+//----------------------------------------------------------------
+function userdefine(user){
+    return new Proxy(user, {
+
+        get(target, prop){
+            console.log(target);   // { name: 'sandeep', class: '5sem' }
+            console.log(prop);     // name, sandeep  or  class, 5sem
+            return target[prop]
+        },
+
+        set(target, prop, value){
+            console.log(target);     // sandeep or 5sem
+            console.log(prop);       // sandeep or 5sem
+            console.log(value);         // sandeep or 5sem
+            
+            target[prop] = value            
+            return true
+        }
+    })
+}
+
+let user = { name: "sandeep", class: "5sem"}
+let newUser = userdefine(user)
+
+
+console.log(newUser.name);
+console.log(newUser.class);
+
 
