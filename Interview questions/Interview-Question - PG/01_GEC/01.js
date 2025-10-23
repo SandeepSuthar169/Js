0deaeacda0097dd471ca0d411c08e0337799eb8a
Sandeep Suthar <ss.suthar.004@gmail.com>
2025-10-23 17:22:42 +0530

    *

---
+++ Interview questions/Interview-Question - PG/01_GEC/01.js
@@ -0,0 +1,10 @@
+console.log(x);
+let x = 10   // Cannot access 'x' before initialization
+
+
+
+// 1/ Globel execution context
+
+// 1 Hoisting :->  define variable (let and const) before to access. 
+// 2 The temporal dead zone (TDZ) :-> let and const create temporal dead zone, any variable who that delear in let and const, verialbel before access tham and throw error that is cannot access variable before initialization
+
